b2c0fd7df778ec340a9780a6e8b14964c781ddf5
Akanksha Akkihal <akankshaakkihal27@gmail.com>
2022-03-26 13:01:47 +0530

    database microservice

--- microservice-2/a2.py
+++ microservice-2/a2.py
@@ -3,14 +3,23 @@ import pika
 import os
 import ast 
 import time as t
+import json 
 host = 'rabbitmq-container'
 queue = os.environ.get('QUEUE_NAME')
 def on_message(ch, method, properties, body):
     message_str = body.decode('UTF-8')
     message=ast.literal_eval(message_str)
-    time = message["time"]
-    t.sleep(time)
-    print(message)
+    key = (message["name"],message["IP"])
+    value=(message["consumer_id"],message["request_IP"])
+    data_dict={}
+    data_dict[str(key)]=str(value)
+    print(data_dict)
+    # Serializing json 
+    json_object = json.dumps(data_dict, indent = 4)
+    
+    # Writing to sample.json
+    with open("db.json", "a") as outfile:
+        outfile.write(json_object)
     
 def main():
     connection_params = pika.ConnectionParameters(host=host)
@@ -21,7 +30,7 @@ def main():
 
     channel.basic_consume(queue=queue, on_message_callback=on_message, auto_ack=True)
 
-    print('Subscribed to ' + queue + ', waiting for messages...')
+    print('Subscribed to data' + queue + ', waiting for messages...')
     channel.start_consuming()
 if __name__ == '__main__':
     main()
